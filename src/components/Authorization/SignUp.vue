ecd51033b442a69b2637e7cca5c1b723b210f287
Vadim Sokoltsov <vforvad@gmail.com>
2019-09-03 21:33:08 +0300

    Rebuild SignUp component

--- src/components/Authorization/SignUp.vue
+++ src/components/Authorization/SignUp.vue
@@ -1,42 +1,63 @@
 <template>
     <form @submit.prevent="submitForm">
-       <b-field 
-                label="Email"
-                :type="{ 'is-danger': $v.email.$invalid && $v.email.$model }"
-                :message="emailErrors">
-                <b-input 
+       <div class="field">
+            <p class="control has-icons-left">
+                <input 
+                    class="input" 
                     type="email" 
-                    v-model="email"
-                    icon-pack="fas"
-                    icon="envelope"
-                    >
-                </b-input>
-            </b-field>
-            <b-field 
-                label="Password"
-                :type="{ 'is-danger': $v.password.$invalid && $v.password.$model }"
-                :message="passwordErrors">
-                <b-input 
+                    placeholder="Email" 
+                    v-model="email" 
+                    v-bind:class="{ 'is-danger': emailErrors.length > 0 }" 
+                    />
+                <span class="icon is-small is-left">
+                    <font-awesome-icon icon="envelope" />
+                </span>
+            </p>
+            <div class="has-text-danger" :if="emailErrors">
+                <ul>
+                    <li v-bind:key="index" v-for="(error, index) in emailErrors">{{ error }}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="field">
+            <p class="control has-icons-left">
+                <input 
+                    class="input" 
                     type="password" 
-                    v-model="password"
-                    icon-pack="fas"
-                    icon="lock"
-                    password-reveal>
-                </b-input>
-            </b-field>
-            <b-field 
-                label="Password confirmation"
-                :type="{ 'is-danger': $v.passwordConfirmation.$invalid && $v.passwordConfirmation.$model }"
-                :message="passwordConfirmationErrors">
-                <b-input 
+                    placeholder="Password" 
+                    v-model="password" 
+                    v-bind:class="{ 'is-danger': passwordErrors.length > 0 }"
+                    />
+                <span class="icon is-small is-left">
+                    <font-awesome-icon icon="lock" />
+                </span>
+            </p>
+            <div class="has-text-danger" :if="passwordErrors">
+                <ul>
+                    <li v-bind:key="index" v-for="(error, index) in passwordErrors">{{ error }}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="field">
+            <p class="control has-icons-left">
+                <input 
+                    class="input" 
                     type="password" 
-                    v-model="passwordConfirmation"
-                    icon-pack="fas"
-                    icon="lock"
-                    password-reveal>
-                </b-input>
-            </b-field>
-            <b-button tag="input" type="is-primary" native-type="submit" value="Sign up" :disabled="$v.$invalid" />
+                    placeholder="Password confirmation" 
+                    v-model="passwordConfirmation" 
+                    v-bind:class="{ 'is-danger': passwordConfirmationErrors.length > 0 }"
+                    />
+                <span class="icon is-small is-left">
+                    <font-awesome-icon icon="lock" />
+                </span>
+            </p>
+            <div class="has-text-danger" :if="passwordConfirmationErrors">
+                <ul>
+                    <li v-bind:key="index" v-for="(error, index) in passwordConfirmationErrors">{{ error }}</li>
+                </ul>
+            </div>
+        </div>
+        <input class="button is-primary" type="submit" value="Sign up" />
     </form>
 </template>
 
@@ -53,24 +74,61 @@ export default {
   },
   computed: {
     emailErrors() {
-        return [ 
-            { 'Field is required': !this.$v.email.required && this.$v.email.$model },
-            { 'Does not match the email': !this.$v.email.email && this.$v.email.$model },
-            ...((this.$store.getters.signUpErrors || {}).email || [])
-        ]
+        let errors = []
+        const serverErrors = ((this.$store.getters.signUpErrors || {}).email || [])
+
+        if (!this.$v.email.required && !!this.$v.email.$model) {
+            errors.push('Field is required')
+        }
+        if (!this.$v.email.email && !!this.$v.email.$model) {
+            errors.push('Does not match the email')
+        }
+        if (serverErrors) {
+            errors = [
+                ...errors,
+                ...serverErrors
+            ]
+        }
+
+        return errors
     },
     passwordErrors() {
-        return [ 
-            { 'Field is required': !this.$v.password.required && this.$v.password.$model },
-            ...((this.$store.getters.signUpErrors || {}).password || [])
-        ]
+        let errors = []
+        const serverErrors = ((this.$store.getters.signUpErrors || {}).password || [])
+
+        if (!this.$v.password.required && !!this.$v.password.$model) {
+            errors.push('Field is required')
+        }
+
+        if (serverErrors) {
+            errors = [
+                ...errors,
+                ...serverErrors
+            ]
+        }
+
+        return errors
     },
     passwordConfirmationErrors() {
-        return [ 
-            { 'Field is required': !this.$v.passwordConfirmation.required && this.$v.passwordConfirmation.$model },
-            { 'Does not match password': !this.$v.passwordConfirmation.sameAsPassword && this.$v.passwordConfirmation.$model },
-            ...((this.$store.getters.signUpErrors || {}).passwordConfirmation || [])
-        ]
+        let errors = []
+        const serverErrors = ((this.$store.getters.signUpErrors || {}).password || [])
+
+        if (!this.$v.passwordConfirmation.required && this.$v.passwordConfirmation.$model) {
+            errors.push('Field is required')
+        }
+
+        if (!this.$v.passwordConfirmation.sameAsPassword && this.$v.passwordConfirmation.$model) {
+            errors.push('Does not match password')
+        }
+
+        if (serverErrors) {
+            errors = [
+                ...errors,
+                ...serverErrors
+            ]
+        }
+
+        return errors
     }
   },
   methods: {
